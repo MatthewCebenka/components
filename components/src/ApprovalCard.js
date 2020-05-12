c416dd9f69ff221a40d4732ebd6d7a62649d6aa2
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-12 04:10:20 -0400

    added jsx to approvalcard

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const ApprovalCard = () => {
-    return (
-
-
-    );
-}
+  return (
+    <div className="ui card">
+      <div className="content">Are you sure?</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basis green button">Approve</div>
+          <div className="ui basis red button">Reject</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ApprovalCard;

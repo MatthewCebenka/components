0ee8b2f6f454caf258d062991fc211183bee73bf
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-12 04:20:03 -0400

    passed commentdetail down to approval card

--- components/src/index.js
+++ components/src/index.js
@@ -2,30 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
-import ApprovalCard from  './ApprovalCard';
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-    <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        date="Today at 4pm"
-        avatar={faker.image.avatar()}
-        comment="Nice blog post!"
-      />
-      <CommentDetail
-        author="Alex"
-        date="Today at 2pm"
-        avatar={faker.image.avatar()}
-        comment="Nice blog post!"
-      />
-      <CommentDetail
-        author="Jane"
-        date="Yesterday at 5pm"
-        avatar={faker.image.avatar()}
-        comment="Nice blog post!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date="Today at 4pm"
+          avatar={faker.image.avatar()}
+          comment="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          date="Today at 2pm"
+          avatar={faker.image.avatar()}
+          comment="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          date="Yesterday at 5pm"
+          avatar={faker.image.avatar()}
+          comment="Nice blog post!"
+        />
+      </ApprovalCard>
     </div>
   );
 };

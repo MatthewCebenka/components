0a475a6b631aef7591463a49519c801594d5c627
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-12 03:53:07 -0400

    added prop for comment

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" date='Today at 4pm' avatar={faker.image.avatar()}/>
-      <CommentDetail author="Alex" date='Today at 2pm' avatar={faker.image.avatar()}/>
-      <CommentDetail author="Jane" date='Yesterday at 5pm' avatar={faker.image.avatar()}/>
+      <CommentDetail
+        author="Sam"
+        date="Today at 4pm"
+        avatar={faker.image.avatar()}
+        comment="Nice blog post!"
+      />
+      <CommentDetail
+        author="Alex"
+        date="Today at 2pm"
+        avatar={faker.image.avatar()}
+        comment="Nice blog post!"
+      />
+      <CommentDetail
+        author="Jane"
+        date="Yesterday at 5pm"
+        avatar={faker.image.avatar()}
+        comment="Nice blog post!"
+      />
     </div>
   );
 };
